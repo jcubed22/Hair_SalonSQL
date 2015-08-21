011465e4ed9db85f99c21386441b2743a84d3f02
Jordan Johansen <jordanjohan@gmail.com>
2015-08-21 09:45:05 -0700

    Create Stylist class.

---
+++ src/Stylist.php
@@ -0,0 +1,89 @@
+<?php
+
+    class Stylist
+    {
+        private $name;
+        private $id;
+    }
+
+        function __construct($name, $id=null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        //Name setter and getter
+        function setName($new_name);
+        {
+            $this->name = $new_name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        //ID getter
+        function getId()
+        {
+            return $this->id;
+        }
+
+        //Save to database
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stylists (name) VALUES ('{$this->getName()}')");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        //Get all stylists
+        static function getAll()
+        {
+            $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists ORDER BY name;");
+            $stylists = array();
+            foreach($returned_stylists as $stylist) {
+                $name = $stylist['name'];
+                $id = $stylist['id'];
+                $new_stylist = new Stylist($name, $id);
+                array_push($stylists, $new_stylist);
+            }
+            return $stylists;
+        }
+
+        //Delete all function, **UPDATE WITH CLIENTS**
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stylists;");
+        }
+
+        //Find function
+        static function find($search_id)
+        {
+            $found_stylist = null;
+            $stylists = Stylist::getAll();
+            foreach($stylists as $stylist) {
+                $stylist_id = $stylist->getId();
+                if ($stylist_id == $search_id) {
+                    $found_stylist = $stylist;
+                }
+            }
+            return $found_stylist;
+        }
+
+        //Update function
+        function update_stylists($new_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE stylists SET name = '{new_name}' WHERE id = {$this->getId()};");
+            $this->setName($new_name);
+        }
+
+        //Delete a Stylist...dun dun DUNNNNN.  **UPDATE FOR CLIENTS**
+        function delete_stylist()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stylists WHERE id = {$this->getId()};");
+        }
+
+
+
+
+?>
