620cce56e3fd43423b546ca76ac6867839b21321
Jordan Johansen <jordanjohan@gmail.com>
2015-08-21 11:44:45 -0700

    Add route for deleting all stylists.

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
 
     require_once __DIR__.'/../vendor/autoload.php';
-    require_once __DIR__.'../src/Stylist.php';
+    require_once __DIR__.'/../src/Stylist.php';
 
     $app = new Silex\Application();
     $app['debug'] = true;
@@ -11,12 +11,25 @@
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
-    use Symfony\Component\HttpFoundation\Request;
-    Request::enableHttpMethodParamterOverride();
+    // use Symfony\Component\HttpFoundation\Request;
+    // Request::enableHttpMethodParamterOverride();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get('/', function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->post('/add_stylist', function() use ($app) {
+        $stylist = new Stylist($_POST['name']);
+        $stylist->save();
+
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->post('/delete_stylists', function() use($app) {
+        Stylist::deleteAll();
+
         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
     });
 
