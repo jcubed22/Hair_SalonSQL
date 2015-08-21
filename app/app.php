a4a273c789fbfb95db9d0462cdb21ff7627b0971
Jordan Johansen <jordanjohan@gmail.com>
2015-08-21 15:44:54 -0700

    Create routes for stylist page.

--- app/app.php
+++ app/app.php
@@ -2,6 +2,7 @@
 
     require_once __DIR__.'/../vendor/autoload.php';
     require_once __DIR__.'/../src/Stylist.php';
+    require_once __DIR__.'/../src/Client.php';
 
     $app = new Silex\Application();
     $app['debug'] = true;
@@ -36,8 +37,21 @@
         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
     });
 
+    $app->get('/stylists/{id}', function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    });
 
+    $app->post('/add_client', function() use ($app) {
+        $c_name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $stylist_id = $_POST['stylist_id'];
+        $client = new Client($c_name, $phone, $id = null, $stylist_id);
+        $client->save();
 
+        $stylist = Stylist::find($stylist_id);
+        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    });
 
     return $app;
 
