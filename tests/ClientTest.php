636624f30fd6b101403968d78ea5a9a8b13d25ca
Jordan Johansen <jordanjohan@gmail.com>
2015-08-21 13:19:23 -0700

    Create test for getting Client Id.  Passed.

---
+++ tests/ClientTest.php
@@ -0,0 +1,55 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Client.php";
+    require_once "src/Stylist.php";
+
+    $server = 'mysql:host=localhost;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class ClientTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Client::deleteAll();
+            Stylist::deleteAll();
+        }
+
+        function test_getId()
+        {
+            //Arrange
+            $name = "Sasha";
+            $id = 2;
+            $test_stylist = new Stylist($name, $id);
+            $test_stylist->save();
+
+            $c_name = "Garry Gergich";
+            $phone = "503-472-8959";
+            $stylist_id = $test_stylist->getId();
+            $test_client = new Client($c_name, $phone, $id, $stylist_id);
+
+            //Act
+            $result = $test_client->getId();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+?>
