54b49b6e3d90e6183906e91241aa0d8740df6885
Jordan Johansen <jordanjohan@gmail.com>
2015-08-21 10:46:42 -0700

    Create app.php to start routing.

---
+++ app/app.php
@@ -0,0 +1,24 @@
+<?php
+
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'../src/Stylist.php';
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $server = 'mysql:host=localhost;dbname=hair_salon';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParamterOverride();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+
+?>
